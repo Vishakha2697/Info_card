2b80585db863d615f0e80d4c396aaa43cec8b41b
Vishakha <40317353+Vishakha2697@users.noreply.github.com>
2019-07-07 14:00:53 -0700

    completed by adding card and details

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,86 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-      MyApp()
-  );
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
-    return  MaterialApp(
+  Widget build(BuildContext  context){
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
-                backgroundColor: Colors.white,
-                backgroundImage: AssetImage('images/smiley.png'),
+                backgroundImage: AssetImage('images/smiley.jpg'),
               ),
               Text('Miss Smiley',
               style: TextStyle(
-                fontSize: 20.0,
+               fontFamily: 'Pacifico',
+                fontSize: 40.0,
+                color: Colors.white,
                 fontWeight: FontWeight.bold,
-              //  fontFamily: ,
               ),
-              ),
-          Text('FLUTTER DEVELOPER',
-            style: TextStyle(
-
-              color: Colors.teal.shade100,
-              fontSize: 20.0,
-              letterSpacing: 2.5,
-              fontWeight: FontWeight.bold,
-            ),
-          ), SizedBox(
-                height: 20.0,
-                width: 150.0,
-                child: Divider(
-                  color: Colors.teal.shade100,
-                ),
-              ),
-
+                 ),
+                 Text('FLUTTER DEVELOPER',
+                 style: TextStyle(
+                   fontFamily: 'Source Sans Pro',
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                 ),
+                 ),
+                 SizedBox(
+                   height: 20.0,
+                   width: 150.0,
+                   child: Divider(
+                     color: Colors.teal.shade100,
+                     ),
+                   ),
+                 Card(
+                   color: Colors.white,
+                  
+                   margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                   child: ListTile(
+                     leading:  Icon(Icons.phone,
+                      color: Colors.red,
+                      ),
+                      title: Text('+91 123 456 7890',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                      ),
+                      ),
+                   ),
+                 ),
+                 Card(
+                   color: Colors.white,
+                  
+                   margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                   child: ListTile(
+                     leading: Icon(Icons.email,
+                      color: Colors.blue,
+                      ),
+                      title:  Text('abc@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                       fontFamily: 'Source Sans Pro',
+                      ),
+                      ),
+                   ),
+                 ),
             ],
-
+            ),
           ),
-        ),
-        ),
-      );
-
-
+        
+      ),
+    );
   }
 }
+
+
